f0c726efed545e8050c5baec887a8a188249fdd6
GurumiTs <86403514+GurumiTs@users.noreply.github.com>
2021-08-10 09:24:12 +0800

    08/10 JDBC file, PropertySource, DataBaseProperties
    1.建立jdbc.properties
    2.Application加入@PropertySource
    3.將DataBaseProperties加入至common.model

--- OnlyFriendsSpringBoot/src/main/java/of/OnlyFriendsSpringBootApplication.java
+++ OnlyFriendsSpringBoot/src/main/java/of/OnlyFriendsSpringBootApplication.java
@@ -3,9 +3,11 @@ package of;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ServletComponentScan
+//@ServletComponentScan
+@PropertySource(value = {"classpath:jdbc.properties"}, ignoreResourceNotFound = true)
 public class OnlyFriendsSpringBootApplication {
 
 	public static void main(String[] args) {
