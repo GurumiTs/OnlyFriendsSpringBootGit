7f71c60ea7ef936ac6ace6cdff0e8a4e3c4e7b0d
GurumiTs <86403514+GurumiTs@users.noreply.github.com>
2021-08-12 23:53:02 +0800

    BlogBean, BlogService, BlogRepository

---
+++ OnlyFriendsSpringBoot/src/main/java/of/blog/model/BlogBean.java
@@ -0,0 +1,131 @@
+package of.blog.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "BlogBean")
+@Component("BlogBean")
+public class BlogBean implements Serializable {
+	static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "ArticleID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer articleID;// 文章ID
+
+	@Column(name = "Title")
+	private String title;// 標題
+
+	@Column(name = "CreateTime")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Timestamp createTime;// PO文日期
+
+	@Column(name = "UpdateTime")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Timestamp updateTime;// 更新日期
+
+	@Column(name = "MainText")
+	private String mainText;// 內容
+
+	@Column(name = "Images")
+	private String images;// 圖片
+
+	@Column(name = "EmpAcc")
+	private String empAcc;// 管理人員ID
+
+	@Column(name = "UserID")
+	private String userID;// PO文者ID
+
+	public Integer getArticleID() {
+		return articleID;
+	}
+
+	public void setArticleID(Integer articleID) {
+		this.articleID = articleID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+
+	public Timestamp getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Timestamp updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public String getMainText() {
+		return mainText;
+	}
+
+	public void setMainText(String mainText) {
+		this.mainText = mainText;
+	}
+
+	public String getImages() {
+		return images;
+	}
+
+	public void setImages(String images) {
+		this.images = images;
+	}
+
+	public String getEmpAcc() {
+		return empAcc;
+	}
+
+	public void setEmpAcc(String empAcc) {
+		this.empAcc = empAcc;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public BlogBean(Integer articleID, String title, Timestamp createTime, Timestamp updateTime, String mainText,
+			String images, String empAcc, String userID) {
+		this.articleID = articleID;
+		this.title = title;
+		this.createTime = createTime;
+		this.updateTime = updateTime;
+		this.mainText = mainText;
+		this.images = images;
+		this.empAcc = empAcc;
+		this.userID = userID;
+	}
+
+	public BlogBean() {
+
+	}
+
+}
---
+++ OnlyFriendsSpringBoot/src/main/java/of/blog/model/BlogService.java
@@ -0,0 +1,39 @@
+package of.blog.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BlogService {
+	
+	private BlogRepository blogRepository;
+	
+	public BlogBean insertBlog(BlogBean bBean) {
+		return blogRepository.save(bBean);
+	}
+	
+	public BlogBean updateBlog(BlogBean bBean) {
+		return blogRepository.save(bBean);
+	}
+	
+	public void deleteById(Integer articleID) {
+		blogRepository.deleteById(articleID);
+	}
+	
+	public BlogBean findById(Integer articleId) {
+		Optional<BlogBean> bRep = blogRepository.findById(articleId);
+		if (bRep.isPresent()) {
+			return bRep.get();
+		}
+		return null;
+	}
+	
+	public List<BlogBean> findAll(){
+		return blogRepository.findAll();
+	}
+	
+}
