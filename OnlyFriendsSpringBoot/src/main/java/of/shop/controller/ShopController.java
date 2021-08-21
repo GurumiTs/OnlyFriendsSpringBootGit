1aa82f7999db0d0c630748aab7f843060733fd58
Elaine-hung <elainhung12@gmail.com>
2021-08-21 16:20:06 +0800

    shopchart-2

--- OnlyFriendsSpringBoot/src/main/java/of/shop/controller/ShopController.java
+++ OnlyFriendsSpringBoot/src/main/java/of/shop/controller/ShopController.java
@@ -1,11 +1,12 @@
 package of.shop.controller;
 
-import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,9 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.multipart.MultipartFile;
 
-import of.coupon.model.Coupon;
 import of.product.model.Product;
 import of.product.model.ProductService;
 
@@ -36,9 +34,16 @@ public class ShopController {
 	private ProductService productService;
 	@Autowired
 	private Product product;
+	
+	@Autowired
+	private static List<Product> list=new ArrayList<Product>();
 
 	@RequestMapping(path = "/addtoshopchart", method = RequestMethod.GET)
-	public String shopCar(@RequestParam(name = "Id") Integer Id, HttpSession session, Model model) {
+	public String shopCar(@RequestParam(name = "Id") Integer Id, HttpServletResponse response,HttpSession session, Model model) {
+		
+		//獲取商品物件
+		Product product=list.get(Id-1);
+		//儲存至購物車
 		Map<Integer, Integer> shopcart = (Map<Integer, Integer>) session.getAttribute("shopcart");
 		// 如果session沒有購物車
 		if (shopcart == null) {
@@ -51,8 +56,28 @@ public class ShopController {
 		} else {
 			shopcart.put(Id, 1);
 		}
-		model.addAttribute("shopcart", shopcart);
-		return "";
+		session.setAttribute("shopcart", shopcart);
+		
+		//為了讓購物車存的時間久一點 現在需要手動設定cookie
+		Cookie cookie =new Cookie("JsessionId", session.getId());
+		cookie.setMaxAge(60*60*24*7);
+		response.addCookie(cookie);
+		
+		//跳轉頁面
+		return "redirect:productpages/transfer";
+	}
+	
+	@RequestMapping(path = "/toCart",method = RequestMethod.GET)
+	public String toCart() {
+		return "productpages/shopcart";
+	}
+	
+	//顯示商品列表
+	@RequestMapping(path="/listproduct")
+	public String listProduct(Model model) {
+		//準備商品 存商品
+		model.addAttribute("list",list);
+		return "list";
 	}
 
 	@RequestMapping(path = "/shopentrypage", method = RequestMethod.GET)
