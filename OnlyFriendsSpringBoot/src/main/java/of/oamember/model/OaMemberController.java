6a905f0e676e9751a52255d8dd5b0cd151f37d17
Regan0857 <87482659+Regan0857@users.noreply.github.com>
2021-08-28 21:08:58 +0800

    Revert "Update OaMemberController.java"
    This reverts commit cf8c378f28837059756fcdfd91a2f55dcfbe3736.

--- OnlyFriendsSpringBoot/src/main/java/of/oamember/model/OaMemberController.java
+++ OnlyFriendsSpringBoot/src/main/java/of/oamember/model/OaMemberController.java
@@ -1,16 +1,7 @@
 package of.oamember.model;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
-import of.officialactive.model.OfficialActive;
 
 @Controller
 @SessionAttributes(names= {"alloaMember"})
@@ -19,12 +10,3 @@ public class OaMemberController {
 	
 
 }
-@GetMapping(path= "/oatojson")
-@ResponseBody
-public Map allOaToJson(Model m) {
-	List<OfficialActive> oaList = officialActiveService.findAll();
-	Map<String, Object> map = new HashMap<>();
-	map.put("data",oaList);
-	return map;
-	
-	
