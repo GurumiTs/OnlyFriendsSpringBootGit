a0e7fb79f6b73bfdea61ba536bead991c8d30137
HollanKao <hollan5835650@gmail.com>
2021-08-31 12:52:11 +0800

    user

--- OnlyFriendsSpringBoot/src/main/java/of/UserActivity/controller/UserActivityController.java
+++ OnlyFriendsSpringBoot/src/main/java/of/UserActivity/controller/UserActivityController.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,8 +23,6 @@ import of.UserActivity.model.UserActivity;
 import of.UserActivity.model.UserActivityService;
 import of.member.model.Member;
 import of.member.model.MemberService;
-import of.officialactive.model.OfficialActive;
-import of.party.model.Party;
 
 @Controller
 @SessionAttributes(names = { "useractivityList" })
@@ -81,26 +77,30 @@ public class UserActivityController {
 	public String userpartyenty(HttpServletRequest request, Model model) {
 		Integer number = Integer.parseInt(request.getParameter("number"));
 		userActivity = userActivityService.select(number);
+		
 		// 進到內頁後see+1
+		userActivity.setSee(userActivity.getSee()+1);
+		System.out.println("-----------------see--------"+userActivity.getSee());
+		userActivityService.updata(userActivity);
 		model.addAttribute("userActivity", userActivity);
 
 		return "useractivepage/useractivepage";
 	}
-	//已報名的內頁 
+
+	// 已報名的內頁
 	@RequestMapping(path = "/useractivity.page2", method = RequestMethod.GET)
 	public String userpartyenty2(HttpServletRequest request, Model model) {
 		Integer number = Integer.parseInt(request.getParameter("number"));
 		userActivity = userActivityService.select(number);
-		// 進到內頁後see+1
 		model.addAttribute("userActivity", userActivity);
 
 		return "useractivepage/useractivepage2";
 	}
+
 	@RequestMapping(path = "/useractivity.page3", method = RequestMethod.GET)
 	public String userpartyenty3(HttpServletRequest request, Model model) {
 		Integer number = Integer.parseInt(request.getParameter("number"));
 		userActivity = userActivityService.select(number);
-		// 進到內頁後see+1
 		model.addAttribute("userActivity", userActivity);
 
 		return "useractivepage/useractivepage3";
@@ -126,19 +126,13 @@ public class UserActivityController {
 	// 會員新增活動
 	@RequestMapping(path = "/userInsertActivity.controller", method = RequestMethod.POST)
 	public String partadd(@RequestParam(name = "cover") MultipartFile cover,
-			@RequestParam(name = "Activityname") String Activityname,
-			@RequestParam(name = "type") String type,
-			@RequestParam(name = "time") String time,
-			@RequestParam(name = "time_up") String time_up,
-			@RequestParam(name = "county") String county,
-			@RequestParam(name = "district") String district,
-			@RequestParam(name = "zipcode") String zipcode,
-			@RequestParam(name = "place") String place,
+			@RequestParam(name = "Activityname") String Activityname, @RequestParam(name = "type") String type,
+			@RequestParam(name = "time") String time, @RequestParam(name = "time_up") String time_up,
+			@RequestParam(name = "county") String county, @RequestParam(name = "district") String district,
+			@RequestParam(name = "zipcode") String zipcode, @RequestParam(name = "place") String place,
 
-			@RequestParam(name = "Detail") String Detail,
-			@RequestParam(name = "condition") String condition,
-			@RequestParam(name = "man") String man,
-			@RequestParam(name = "woman") String woman,
+			@RequestParam(name = "Detail") String Detail, @RequestParam(name = "condition") String condition,
+			@RequestParam(name = "man") String man, @RequestParam(name = "woman") String woman,
 			HttpServletRequest request, Model m)
 			throws SQLException, IllegalStateException, IOException, NullPointerException {
 
@@ -275,50 +269,48 @@ public class UserActivityController {
 	}
 
 	// 會員參加活動
-	@RequestMapping(path = "/addactivity",method = RequestMethod.GET)
+	@RequestMapping(path = "/addactivity", method = RequestMethod.GET)
 //	@ResponseBody
-	public String addactivity(HttpServletRequest request,@RequestParam(name = "number") Integer number) {
+	public String addactivity(HttpServletRequest request, @RequestParam(name = "number") Integer number) {
 
 		Member m1 = (Member) request.getSession().getAttribute("personalinfo");
 		String memberAccount = m1.getMemberAccount();
 //		add member
 		Member m2 = memberService.findByMemberAccount(memberAccount);
-		
-		
-		//尋找活動
+
+		// 尋找活動
 		UserActivity ua = userActivityService.select(number);
-		
-		
+
 		List<Member> par = ua.getParticipate();
 		par.add(m2);
 		userActivityService.updata(ua);
-		
-		
-		//判斷是否增加成功
+
+		// 判斷是否增加成功
 		return "useractivepage/usermgmtjson";
 	}
+
 	// 會員查詢自己參加活動
 //	抓參加者值
-	@RequestMapping(path = "/AlreadyParticipated",method = RequestMethod.POST)
+	@RequestMapping(path = "/AlreadyParticipated", method = RequestMethod.POST)
 	@ResponseBody
 	public List<UserActivity> userparty(Model model, HttpServletRequest request) {
-		
-		//先找到使用著帳號
+
+		// 先找到使用著帳號
 		Member m1 = (Member) request.getSession().getAttribute("personalinfo");
 		String memberAccount = m1.getMemberAccount();
 		System.out.println("會員參加活動");
 //		使用帳號在 participate 找到使用著有參加哪些活動
-		List<Integer> activitynumber=userActivityService.findByparticipate(Integer.parseInt(memberAccount));
-		
-		System.out.println("number"+activitynumber);
+		List<Integer> activitynumber = userActivityService.findByparticipate(Integer.parseInt(memberAccount));
+
+		System.out.println("number" + activitynumber);
 		List<UserActivity> activity = new ArrayList<UserActivity>();
-		
-		for(Integer num : activitynumber) {
-		UserActivity ac = userActivityService.select(num);
-		activity.add(ac);
+
+		for (Integer num : activitynumber) {
+			UserActivity ac = userActivityService.select(num);
+			activity.add(ac);
 		}
 //		userActivityService.f
-				
+
 		return activity;
 	}
 //	System.out.println("step1");
