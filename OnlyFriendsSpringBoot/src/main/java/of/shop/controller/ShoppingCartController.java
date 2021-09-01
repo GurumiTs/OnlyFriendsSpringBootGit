4954b123e90b0a67064023280d56b338f52d5619
Elaine-hung <elainhung12@gmail.com>
2021-09-02 03:56:24 +0800

    littlechange

--- OnlyFriendsSpringBoot/src/main/java/of/shop/controller/ShoppingCartController.java
+++ OnlyFriendsSpringBoot/src/main/java/of/shop/controller/ShoppingCartController.java
@@ -1,32 +1,16 @@
 package of.shop.controller;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
-import javax.mail.Flags.Flag;
-import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.jsoup.Connection;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.security.core.authority.mapping.MapBasedAttributes2GrantedAuthoritiesMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,15 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
-import com.google.gson.JsonArray;
-import com.nimbusds.jose.shaded.json.JSONArray;
-
 import of.member.model.MemberService;
 import of.product.model.Product;
 import of.product.model.ProductService;
 import of.shop.model.CartItem;
-import of.shop.model.CartResponse;
 
 @Controller
 @SessionAttributes(names = { "cartlist" })
